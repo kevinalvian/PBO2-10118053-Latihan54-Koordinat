3b7dbb139a57a4a1cd3b880a9cc5a83da3f3e8d8
kevinalvian <55798459+kevinalvian@users.noreply.github.com>
2019-11-07 22:53:52 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118053/latihan54/koordinat/warnakordinat.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118053.latihan54.koordinat;
+
+/**
+ *
+ * @author ACER
+ */
+public class warnakordinat extends Koordinat{
+    public  String namawarna;
+
+    public warnakordinat(String namawarna, int x, int y) {
+        super(x, y);
+        this.namawarna = namawarna;
+    }
+
+    public String getNamawarna() {
+        return namawarna;
+    }
+
+    public void setNamawarna(String namawarna) {
+        this.namawarna = namawarna;
+    }
+    
+}
